5f1d28adabecc2dab8ad9b9620268cd9036e613b
shypang <smartdragon417@gmail.com>
2023-05-19 19:44:52 +0900

    Del mongoDB shell

--- mongoDB_shell/mongo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#/home/robertseo/mongo/data/db
-
-
-docker run --name mongodb -v /home/robertseo/mongo/data/db:/data/db -d -p 27017:27017 mongo
--- mongoDB_shell/nosqlbooster_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/robertseo/mongo/nosqlbooster4mongo-8.0.10/nosqlbooster4mongo
