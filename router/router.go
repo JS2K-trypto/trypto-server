a1d1a45c7e22d18a94ea86a17dd4ff59c1b4e024
shypang <smartdragon417@gmail.com>
2023-06-05 17:15:41 +0900

    Adding : contract ver 0.6

--- router/router.go
+++ router/router.go
@@ -30,11 +30,11 @@ func CORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		//허용할 header 타입에 대해 열거
-		//c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Forwarded-For, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Forwarded-For, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		//c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		//허용할 method에 대해 열거
-		//c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		//c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
